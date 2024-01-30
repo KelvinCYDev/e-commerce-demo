e963756566b168552d337f960d01332fc69e2408
KelvinCY <kelvincyhk@gmail.com>
2024-01-30 00:39:53 -0800

    public version

---
+++ src/payload/graphics/Icon/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './index.scss'
+
+export const Icon = () => (
+  <div className="icon">
+    <picture>
+      <img src="/logo.png" alt="Logo" />
+    </picture>
+  </div>
+)
---
+++ src/payload/graphics/Logo/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './index.scss'
+
+export const Logo = () => (
+  <div className="logo">
+    <picture>
+      <img src="/logo.png" alt="Logo" />
+    </picture>
+  </div>
+)
